a7c36d5226a368b8311208fc2ac9260098903170
Sshashank5690 <78215769+Sshashank5690@users.noreply.github.com>
2023-11-13 18:51:39 +0530

    initial v0.02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
+import MaxWidthWrapper from "@/components/MaxWidthWrapper"
 
 export default function Home() {
   return (
-    <div></div>
+    <MaxWidthWrapper><div></div></MaxWidthWrapper>
   )
 }
