3555ea03eadecebf78eee33dfee42aaf026e495d
Sshashank5690 <78215769+Sshashank5690@users.noreply.github.com>
2023-11-16 19:31:37 +0530

    v0.04

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,15 @@ const config: Config = {
       "8xl": "1408px"
     },
     colors: {
-      border: "hsl()"
+      border: "hsl(var(--border))",
+      input: "hsl(var(--input))",
+      ring: "hsl(var(--ring))",
+      background: "hsl(var(--background))",
+      foreground: "hsl(var(--foreground))",
+      primary: {
+        DEFAULT: "hsl(var(--primary))",
+        foreground: "hsl(var(--primary-foreground))",
+      }
     },
   },
 },
