3ad1d721c70ad8fd7fbdd67917d90ab5c07cfd13
Sshashank5690 <78215769+Sshashank5690@users.noreply.github.com>
2023-12-13 00:59:23 +0530

    v0.06

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
 import Link from "next/link"
+import { ArrowRight } from "lucide-react"
 
 export default function Home() {
   return (
@@ -19,7 +20,7 @@ export default function Home() {
       </p>
 
       <Link href='/dashboard' target='_blank'>
-        Get started
+        Get started <ArrowRight className="ml-2 h-5 w-5" />
       </Link>
     </MaxWidthWrapper>
   )
