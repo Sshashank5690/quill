75b5b7b3e6a9edea112ecd14ff9913a8d103283f
Sshashank5690 <78215769+Sshashank5690@users.noreply.github.com>
2024-01-23 00:33:01 +0530

    Navigation section added and also integrated the auth using kindeAuth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         'min-h-screen font-sans antialiased grainy',
         inter.className
       )}>
+      <Navbar />
       {children}
       </body>
     </html>
